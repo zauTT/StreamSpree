ce292715c70d1ff0a2eb7eec53703a9f36a43d7d
zauTT <giozau1@gmail.com>
2025-05-30 15:08:20 +0400

    added scrollView to MovieDetailViewController so long descriptions don’t get cut off on smaller screens

--- StreamSpree/ViewControllers/MovieDetailViewController.swift
+++ StreamSpree/ViewControllers/MovieDetailViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
     private let movie: Movie
+
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }()
     
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     private let posterImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
@@ -18,16 +30,16 @@ class MovieDetailViewController: UIViewController {
         iv.translatesAutoresizingMaskIntoConstraints = false
         return iv
     }()
-    
+
     private let genreLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16, weight: .regular)
-        label.numberOfLines = 1
+        label.numberOfLines = 0
         label.textColor = .secondaryLabel
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     private let ratingLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16, weight: .regular)
@@ -35,7 +47,7 @@ class MovieDetailViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     private let overviewLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16)
@@ -44,56 +56,68 @@ class MovieDetailViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     init(movie: Movie) {
         self.movie = movie
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = movie.title
-        
+
         setupLayout()
         configureView()
     }
-    
+
     private func setupLayout() {
-        view.addSubview(posterImageView)
-        view.addSubview(genreLabel)
-        view.addSubview(ratingLabel) 
-        view.addSubview(overviewLabel)
-        
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+
+        [posterImageView, genreLabel, ratingLabel, overviewLabel].forEach { contentView.addSubview($0) }
+
         NSLayoutConstraint.activate([
-            posterImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
-            posterImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+
+            posterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            posterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             posterImageView.widthAnchor.constraint(equalToConstant: 150),
             posterImageView.heightAnchor.constraint(equalToConstant: 250),
-            
-            genreLabel.leadingAnchor.constraint(equalTo: posterImageView.trailingAnchor, constant: 10),
-            genreLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 130),
-            genreLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            
-            ratingLabel.topAnchor.constraint(equalTo: genreLabel.bottomAnchor, constant: 30),
-            ratingLabel.leadingAnchor.constraint(equalTo: genreLabel.leadingAnchor),
-            ratingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
 
-            overviewLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 10),
-            overviewLabel.leadingAnchor.constraint(equalTo: posterImageView.leadingAnchor),
-            overviewLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+            genreLabel.topAnchor.constraint(equalTo: posterImageView.topAnchor),
+            genreLabel.leadingAnchor.constraint(equalTo: posterImageView.trailingAnchor, constant: 16),
+            genreLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+
+            ratingLabel.topAnchor.constraint(equalTo: genreLabel.bottomAnchor, constant: 8),
+            ratingLabel.leadingAnchor.constraint(equalTo: genreLabel.leadingAnchor),
+            ratingLabel.trailingAnchor.constraint(equalTo: genreLabel.trailingAnchor),
+
+            overviewLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 16),
+            overviewLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            overviewLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            overviewLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20)
         ])
     }
-    
+
     private func configureView() {
         genreLabel.text = "Genres: " + movie.genreNames.joined(separator: ", ")
         ratingLabel.text = "⭐️ \(movie.voteAverage)"
         overviewLabel.text = "Description: \(movie.overview)"
-        
+
         if let url = movie.posterURL {
             URLSession.shared.dataTask(with: url) { data, _, _ in
                 if let data = data, let image = UIImage(data: data) {
@@ -101,7 +125,7 @@ class MovieDetailViewController: UIViewController {
                         self.posterImageView.image = image
                     }
                 }
-            } .resume()
+            }.resume()
         } else {
             posterImageView.image = nil
         }
